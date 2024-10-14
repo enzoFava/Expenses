aa49f63a0849d6b2a559ea76e9a4d4a21932bab2
enzoFava <117693440+enzoFava@users.noreply.github.com>
2024-10-14 13:57:54 -0300

    debug

--- server/build_files.sh
+++ server/build_files.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-python3.9 manage.py collectstatic --noinput --clear
+pip install --upgrade pip
+pip install -r requirements.txt
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput --clear
 echo "BUILD END"
